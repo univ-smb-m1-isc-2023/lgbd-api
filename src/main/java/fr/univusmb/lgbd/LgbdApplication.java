b3bd2cccd89e86d227618fbfc86f50027005ec26
Martin ARNOUX <47914268+Skelzu@users.noreply.github.com>
2024-02-09 18:01:51 +0100

    init commit

---
+++ src/main/java/fr/univusmb/lgbd/LgbdApplication.java
@@ -0,0 +1,13 @@
+package fr.univusmb.lgbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univusmb/lgbd/LgbdApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univusmb.lgbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
