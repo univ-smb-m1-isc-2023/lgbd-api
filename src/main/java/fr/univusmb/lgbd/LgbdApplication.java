cc8b22829475fdff497a968a61a547ad3455b892
Martin ARNOUX <arnouxmartin@proton.me>
2024-02-23 16:05:38 +0100

    add import

--- src/main/java/fr/univusmb/lgbd/LgbdApplication.java
+++ src/main/java/fr/univusmb/lgbd/LgbdApplication.java
@@ -2,6 +2,7 @@ package fr.univusmb.lgbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class LgbdApplication {
