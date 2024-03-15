67167f2f569544423abf4235a7e2315366a7539a
Emilien39110 <emilien.boitouzet@hotmail.fr>
2024-03-15 17:22:03 +0100

    Add connexion database

--- src/main/java/fr/univusmb/lgbd/HelloController.java
+++ src/main/java/fr/univusmb/lgbd/HelloController.java
@@ -1,14 +1,35 @@
 package fr.univusmb.lgbd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
 @RestController
 public class HelloController {
+//    @Autowired
+    private DataSource dataSource ;
 
     @GetMapping("/hello")
-    public String index() {
+    public String hello() {
         return "Hello World!";
     }
+
+//    public DataSource getDataSource() {
+//        return dataSource;
+//    }
+
+    @GetMapping("/base")
+    public String base(){
+        System.out.println(dataSource);
+        if(dataSource !=null) {
+            return "Hello base de données !";
+        }else{
+            return "Echec bdd ";
+        }
+//        return "ok ok ";
+    }
+
 
 }
