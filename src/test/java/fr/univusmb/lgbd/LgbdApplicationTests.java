e2a755c9748a8c624f1ad63e7faeec8a3316d732
Martin ARNOUX <arnouxmartin@proton.me>
2024-03-15 14:17:54 +0100

    fix mapping hello word

--- src/test/java/fr/univusmb/lgbd/LgbdApplicationTests.java
+++ src/test/java/fr/univusmb/lgbd/LgbdApplicationTests.java
@@ -1,13 +1,25 @@
 package fr.univusmb.lgbd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LgbdApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	void testHelloMapping() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World!"));
+	}
 }
