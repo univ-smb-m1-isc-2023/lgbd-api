d3872c4a7405412746c021d1c2432e6c6baf1901
Martin ARNOUX <arnouxmartin@proton.me>
2024-03-15 14:17:54 +0100

    fix mapping hello word

--- src/main/java/fr/univusmb/lgbd/LgbdApplication.java
+++ src/main/java/fr/univusmb/lgbd/LgbdApplication.java
@@ -2,7 +2,12 @@ package fr.univusmb.lgbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class LgbdApplication {
@@ -11,9 +16,19 @@ public class LgbdApplication {
 		SpringApplication.run(LgbdApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
-	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
