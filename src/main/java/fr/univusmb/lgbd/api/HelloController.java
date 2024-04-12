59716f0d87945a9c9e8af36d13b7a746c1922b1d
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-04-12 14:11:42 +0200

    added route for creation of users

--- src/main/java/fr/univusmb/lgbd/api/HelloController.java
+++ src/main/java/fr/univusmb/lgbd/api/HelloController.java
@@ -1,9 +1,14 @@
 package fr.univusmb.lgbd.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -31,5 +36,27 @@ public class HelloController {
             return "Echec bdd ";
         }
         // return "ok ok ";
+    }
+
+    @GetMapping("/getUsers")
+    public List<User> getUsers() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String sql = "SELECT * FROM users";
+
+        List<User> users = jdbcTemplate.query(sql, (rs, rowNum) -> {
+            User user = new User();
+            user.setId(rs.getLong("identifiant"));
+            user.setName(rs.getString("nom"));
+            user.setEmail(rs.getString("email"));
+            return user;
+        });
+        return users;
+    }
+
+    @PostMapping("/addUser")
+    public void addUser(@RequestBody User user) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String sql = "INSERT INTO users (nom, email, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getPassword());
     }
 }
--- src/main/java/fr/univusmb/lgbd/api/User.java
+++ src/main/java/fr/univusmb/lgbd/api/User.java
@@ -19,12 +19,16 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
+    private String password;
+
     public User() {
     }
 
-    public User(String name, String email) {
+    public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     public String getName() {
@@ -34,4 +38,29 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
 }
