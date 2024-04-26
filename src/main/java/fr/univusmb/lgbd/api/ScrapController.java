5173b938319ab197712e385741f26288c4fd8bb4
Emilien39110 <emilien.boitouzet@hotmail.fr>
2024-04-26 09:00:47 +0200

    Update ScrapController.java

--- src/main/java/fr/univusmb/lgbd/api/ScrapController.java
+++ src/main/java/fr/univusmb/lgbd/api/ScrapController.java
@@ -1,12 +1,7 @@
 package fr.univusmb.lgbd.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 
