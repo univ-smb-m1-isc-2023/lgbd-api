fab9cb10763a9455b7cf32650a367a3260a8e77f
Emilien39110 <emilien.boitouzet@hotmail.fr>
2024-04-26 08:58:53 +0200

    Update ScrapController.java

--- src/main/java/fr/univusmb/lgbd/api/ScrapController.java
+++ src/main/java/fr/univusmb/lgbd/api/ScrapController.java
@@ -13,10 +13,17 @@ import org.springframework.http.ResponseEntity;
 @RestController
 @CrossOrigin(origins = { "*" })
 public class ScrapController {
+    private String scrap;
 
     @PostMapping("/scrap")
     public ResponseEntity<Void> scrap(@RequestBody String body) {
         System.out.println("Scrap : " + body);
+        this.scrap = body;
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/scrap")
+    public String getScrap() {
+        return this.scrap;
     }
 }
